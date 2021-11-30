db896401c20fbdcbbbb0875e2535d1d9b73296ca
specnazm <laketic.milena98@gmail.om>
2021-11-30 14:40:30 +0100

    Added counter serializer

--- employee/serializer.py
+++ employee/serializer.py
@@ -1,9 +1,14 @@
 from django.db.models import fields
 from rest_framework import serializers
-from .models import Employee
+from .models import Employee, Counter
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = ['id', 'name', 'surname', 'salary', 'birth_date']
+
+class CounterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Counter
+        fields = ['value']
