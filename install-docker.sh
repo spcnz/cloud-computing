bb66d5665547555be4fa5df47d7dbf209325033f
Milena Laketic <laketic.milena98@gmail.com>
2022-01-07 22:11:45 +0100

    Added check in docker-install script

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+if [ -x "$(command -v docker)" ];
+then 
+    echo "Docker is already installed!"
+    exit
+else
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
